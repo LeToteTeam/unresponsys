4a811d1f56312665e620b26523d280ca55711e11
Kevin Kimball <kwkimball@gmail.com>
2015-11-27 13:06:22 -0800

    more folder tests

--- spec/folder_spec.rb
+++ spec/folder_spec.rb
@@ -7,4 +7,15 @@ describe Unresponsys::Folder do
     expect(folder).to be_an_instance_of(Unresponsys::Folder)
   end
 
+  context do
+    before(:each) do
+      @folder = Unresponsys::Folder.find('TestData')
+    end
+
+    it '#tables.find returns an instance of Table' do
+      table = @folder.tables.find('MyTable')
+      expect(table).to be_an_instance_of(Unresponsys::Table)
+    end
+  end
+
 end
