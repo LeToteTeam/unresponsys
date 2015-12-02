39f7dae83a46dd05fbb5539e05285bc9d130a35e
Kevin Kimball <kwkimball@gmail.com>
2015-12-01 17:15:42 -0800

    fix non-thread-safe thing

--- lib/unresponsys/client.rb
+++ lib/unresponsys/client.rb
@@ -5,53 +5,37 @@ class Unresponsys
     include HTTParty
 
     def initialize(options = {})
-      @@username ||= options[:username]
-      @@password ||= options[:password]
+      raise Unresponsys::ArgumentError, 'Username is required' unless options[:username]
+      @username = options[:username]
+
+      raise Unresponsys::ArgumentError, 'Password is required' unless options[:password]
+      @password = options[:password]
+
       raise Unresponsys::Error, 'Could not authenticate' unless authenticate
+
       self.class.debug_output($stdout) if options[:debug]
     end
 
     def self.get(path, options = {}, &block)
-      begin
-        r = perform_request Net::HTTP::Get, path, options, &block
-        handle_error(r)
-      rescue Unresponsys::Retry
-        r = perform_request Net::HTTP::Get, path, options, &block
-        handle_error(r)
-      end
+      r = perform_request Net::HTTP::Get, path, options, &block
+      handle_error(r)
     end
 
     def self.post(path, options = {}, &block)
-      begin
-        r = perform_request Net::HTTP::Post, path, options, &block
-        handle_error(r)
-      rescue Unresponsys::Retry
-        r = perform_request Net::HTTP::Post, path, options, &block
-        handle_error(r)
-      end
+      r = perform_request Net::HTTP::Post, path, options, &block
+      handle_error(r)
     end
 
     def self.delete(path, options = {}, &block)
-      begin
-        r = perform_request Net::HTTP::Delete, path, options, &block
-        handle_error(r)
-      rescue Unresponsys::Retry
-        r = perform_request Net::HTTP::Delete, path, options, &block
-        handle_error(r)
-      end
+      r = perform_request Net::HTTP::Delete, path, options, &block
+      handle_error(r)
     end
 
     private
 
     def self.handle_error(response)
       if response.is_a?(Hash) && response.keys.include?('errorCode')
-        if response['title'].include?('token expired')
-          self.new
-          raise Unresponsys::Retry
-        end
-        if response['title'].include?('not found')
-          raise Unresponsys::NotFoundError, response['detail']
-        end
+        raise Unresponsys::NotFoundError, response['detail'] if response['title'].include?('not found')
         raise Unresponsys::Error, "#{response['title']} - #{response['detail']}"
       end
       response
@@ -59,7 +43,7 @@ class Unresponsys
 
     def authenticate
       self.class.headers('Content-Type' => 'application/x-www-form-urlencoded')
-      body = { user_name: @@username, password: @@password, auth_type: 'password' }
+      body = { user_name: @username, password: @password, auth_type: 'password' }
       r = self.class.post('https://login2.responsys.net/rest/api/v1/auth/token', body: body)
       return false unless r.success?
       self.class.headers('Authorization' => r['authToken'], 'Content-Type' => 'application/json')
--- lib/unresponsys/errors.rb
+++ lib/unresponsys/errors.rb
@@ -2,5 +2,4 @@ class Unresponsys
   class ArgumentError < StandardError; end
   class Error < StandardError; end
   class NotFoundError < StandardError; end
-  class Retry < StandardError; end
 end
