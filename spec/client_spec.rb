3618a3b917913d282b6c4eeabf28a371fb672b52
Kevin Kimball <kwkimball@gmail.com>
2016-06-08 11:12:45 -0700

    add tests

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,13 +1,25 @@
 require 'spec_helper'
 
 describe Unresponsys::Client do
-  it '#authenticate is success' do
-    VCR.use_cassette('authenticate_client') do
-      expect {
-        @client = Unresponsys::Client.new(username: ENV['R_USER'], password: ENV['R_PASS'])
-      }.not_to raise_error
-      expect(@client.token).not_to be_nil
-      expect(@client.endpoint).not_to be_nil
+  context 'with valid username and password' do
+    it 'sets token and endpoint' do
+      VCR.use_cassette('authenticate_client') do
+        expect {
+          @client = Unresponsys::Client.new(username: ENV['R_USER'], password: ENV['R_PASS'])
+        }.not_to raise_error
+        expect(@client.token).not_to be_nil
+        expect(@client.endpoint).not_to be_nil
+      end
+    end
+  end
+
+  context 'without username and password' do
+    it 'raises an error' do
+      VCR.use_cassette('authenticate_client_invalid') do
+        expect {
+          @client = Unresponsys::Client.new
+        }.to raise_error
+      end
     end
   end
 end
